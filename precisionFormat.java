c39309c4195cf9df9ce0639a8b6e7208872124db
Rajesh Borate <rajeshborate08@gmail.com>
2020-03-06 19:55:18 +0530

    Create precisionFormat.java

---
+++ precisionFormat.java
@@ -0,0 +1,16 @@
+//User function Template for Java
+
+class Geeks{
+    
+    static void printInFormat(float a, float b){
+        float result = a/b;
+        
+        
+        System.out.printf("%.7f %.3f", result, result);
+        System.out.println();
+        
+        // Your code here to print upto 3 decimal places
+        
+    }    
+    
+}
