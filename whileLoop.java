d8fd4822624dab3565bb784d3e1d1af17f713e39
Rajesh Borate <rajeshborate08@gmail.com>
2020-03-09 17:24:08 +0530

    Create whileLoop.java

---
+++ whileLoop.java
@@ -0,0 +1,14 @@
+//User function Template for Java
+class Geeks {
+	 static void printTable (int n) 
+	 {
+            int multiplier=10;
+            while(multiplier>0)
+            {
+                System.out.print(n*multiplier+" ");
+                multiplier--;
+            }
+       System.out.println();
+	 }
+
+}
