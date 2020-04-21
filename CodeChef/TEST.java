78b968c6ce315ef628d470915d89b005067ca3e6
Rajesh Borate <rajeshborate08@gmail.com>
2020-04-21 12:31:43 +0530

    Create TEST.java

---
+++ CodeChef/TEST.java
@@ -0,0 +1,23 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc = new Scanner(System.in);
+	    int a = 100000;
+	    while(a>0){
+	    int n = sc.nextInt();
+		if(n!=42)
+		    System.out.println(n);
+		else
+		    break;
+		a--;
+	    }
+	}
+}
