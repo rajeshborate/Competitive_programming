7503aa502da53e7890f817ac19ff9e6c04d25701
Rajesh Borate <rajeshborate08@gmail.com>
2020-04-26 07:39:23 +0530

    Create CHEFSTUD.java

---
+++ CodeChef/CHEFSTUD.java
@@ -0,0 +1,30 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t =sc.nextInt();
+		
+		while(t-->0){
+		    String s = sc.next();
+		    int count = 0;
+		    for(int i=0;i<s.length()-1; i++){
+		        if((s.charAt(i)=='<')&& (s.charAt(i+1)=='>')){
+		            count++;
+		        }
+		        
+		    }
+		    System.out.println(count);
+		    
+		}
+		
+		
+	}
+}
